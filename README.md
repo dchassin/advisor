21064f722b4570488cc3089bcb012e58f607906f
David P. Chassin <dchassin@slac.stanford.edu>
2023-01-11 15:45:15 -0800

    Setup for mac conversation

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# advisor
- 
+# Conversational front-end for OpenAI Davinci
+
+## Preparation on Mac
+
+In a terminal window, do the following:
+
+1. `brew install portaudio`
+2. `pip install -r requirements.txt`
+3. `python3 -m advise --signup` to get your OpenAI API key (copy it)
+4. `python3 -m advise --apikey=PASTE_YOUR_KEY_HERE`
+
+## Configuration
+
+1. Edit `config.json` and set your name, and the Apply voice you want the speaker to use.
+
+## Running
+
+1. Run `python3 -m speak` in a terminal. Give the terminal permission to access your microphone
+2. When 'Listening...' is displayed, you may ask your question.
+3. Be patient, neither the Google recognizer nor the OpenAI ChatGPT system is fast.
--- speak.py
+++ speak.py
@@ -3,59 +3,72 @@
 #
 # Mac install
 # $ brew install portaudio
-# $ pip install pyaudio
-# $ pip install pyttsx3
-# $ pip install speechrecognition
+# $ pip install -r requirements.txt
  
-import os
+import os, json
 import speech_recognition as sr
 import pyttsx3
+import advise
+
+advise.FORCE = True
+
+try:
+    with open(f"config.json","r") as fh:
+        config = json.load(fh)
+except Exception as err:
+    print("ERROR:",err)
+    config = {
+        "name" : os.getenv('USER').title(),
+        "voice" : "Alex"
+    }
+NAME = config["name"]
+VOICE = config["voice"]
  
 # Initialize the recognizer
 r = sr.Recognizer()
  
 # Function to convert text to
 # speech
+engine = pyttsx3.init()
+engine.setProperty('voice',f"com.apple.speech.synthesis.voice.{VOICE}")
 def SpeakText(command):
      
     # Initialize the engine
-    engine = pyttsx3.init()
     engine.say(command)
     engine.runAndWait()
      
-     
-# Loop infinitely for user to
-# speak
- 
-while(1):   
-     
-    # Exception handling to handle
-    # exceptions at the runtime
-    try:
-         
-        # use the microphone as source for input.
-        with sr.Microphone() as source:
-             
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level
-            r.adjust_for_ambient_noise(source, duration=1)
-             
-            SpeakText(f"Hello {os.getenv('USER').title()}")
+with sr.Microphone() as source:
 
-            #listens for the user's input
-            audio = r.listen(source)
-            print("...",flush=True)
-             
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio)
-            MyText = MyText.lower()
- 
-            print(f"Did you say {MyText}?",flush=True)
-            SpeakText(MyText)
-             
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
+    print("Calibrating...",flush=True)     
+    r.adjust_for_ambient_noise(source, duration=1)
+     
+    SpeakText(f"Hello {NAME}")
+
+    while(1):   
          
-    except sr.UnknownValueError:
-        print("unknown error occurred")
+        try:
+             
+            # use the microphone as source for input.
+            
+            print("Listening...",flush=True)
+            audio = r.listen(source)
+             
+            print("Recognizing...",flush=True)
+            query = r.recognize_google(audio)
+
+            if query.lower() == "goodbye":
+                break
+
+            print("Thinking...",flush=True)
+            reply = advise.query(query)
+ 
+            print(f"Replying...",flush=True)
+            SpeakText(reply)
+             
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+             
+        except sr.UnknownValueError:
+            print("unknown error occurred")
+
+SpeakText(f"Goodbye {NAME}")
